54ed444baa1d4c925b7eebbb79d8eae09a7f2349
Unknown <talkwithpradeep@gmail.com>
2017-06-24 02:27:36 -0500

    refactor(module-rest):remove spring boot app

--- rest/WeatherApp-boot/src/main/java/egenproj/WeatherApp/Controller/Constants/Mapper.java
+++
@@ -1,8 +0,0 @@
-package egenproj.WeatherApp.Controller.Constants;
-
-public final class Mapper {
-public static final String WEATHER="weather";
-public static final String CITY="city";
-
-
-}
--- rest/WeatherApp-boot/src/main/java/egenproj/WeatherApp/Service/WindService.java
+++
@@ -1,9 +0,0 @@
-package egenproj.WeatherApp.Service;
-
-import egenproj.WeatherApp.Entity.Wind;
-
-public interface WindService {
-
-	public Wind saveWind(Wind wind);
-	
-}
