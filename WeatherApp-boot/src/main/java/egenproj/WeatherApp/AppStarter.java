c9087d7142acfd8d4017a2a9338f5c28dbcd17bd
Unknown <talkwithpradeep@gmail.com>
2017-06-24 21:07:23 -0500

    feat(module-container):add prod config file to spring boot application

--- WeatherApp-boot/src/main/java/egenproj/WeatherApp/AppStarter.java
+++ WeatherApp-boot/src/main/java/egenproj/WeatherApp/AppStarter.java
@@ -4,15 +4,18 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Import;
 import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.AbstractEnvironment;
 
 import egenproj.WeatherApp.config.ApplicationConfig;
 import egenproj.WeatherApp.config.SwaggerConfig;
 
-@PropertySource(value="classpath:app.properties")
+//@PropertySource(value="classpath:app.properties")
 @SpringBootApplication
 @Import({ApplicationConfig.class,SwaggerConfig.class})
 public class AppStarter {
 public static void main(String[] args) {
+	System.setProperty(AbstractEnvironment.ACTIVE_PROFILES_PROPERTY_NAME, "prod");
 	SpringApplication.run(AppStarter.class, args);
+	
 }
 }
